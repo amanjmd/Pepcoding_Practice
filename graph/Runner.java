a9e6ba538c0e644c9c8cb147cf4c1ae8c8810d53
aman_kapoor <ak.2090@outlook.com>
2022-08-15 16:36:03 +0530

    Added Multisolver:JustLargest/JustSmallest

--- graph/Runner.java
+++ graph/Runner.java
@@ -6,13 +6,30 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 import graph.Graphs.Edge;
+/*
+1. You are given a graph, a src vertex and a destination vertex.
+2. You are give a number named "criteria" and a number "k".
+3. You are required to find and print the values of 
+3.1 Smallest path and it's weight separated by an "@"
+3.2 Largest path and it's weight separated by an "@"
+3.3 Just Larger path (than criteria in terms of weight) and it's weight separated by an "@"
+3.4 Just smaller path (than criteria in terms of weight) and it's weight separated by an "@"
+3.5 Kth largest path and it's weight separated by an "@"
 
+https://nados.io/question/multisolver-smallest-longest-ceil-floor-kthlargest-path?zen=true
+
+*/
 public class Runner {
     static int smallestWeight = Integer.MAX_VALUE;
     static String pathWithSmallestWeight;
     static int largestWeight = Integer.MIN_VALUE;
     private static String pathWithLargesttWeight;
     static PriorityQueue<Pair> pq = null;
+    static String pathJustLargest;
+    static int weightJustLargest= Integer.MAX_VALUE;
+    static String pathJustSmallest;
+    static int weightJustSmallest= Integer.MIN_VALUE;
+
     static class Pair implements Comparable<Pair> {
         int wsf;
         String psf;
@@ -35,15 +52,20 @@ public class Runner {
         // Has Path
         int src = Integer.parseInt(br.readLine());
         int dest = Integer.parseInt(br.readLine());
+        int criteria = Integer.parseInt(br.readLine());
         int kForkLargest = Integer.parseInt(br.readLine());
         boolean[] visited = new boolean[graph.length];
         // System.out.println(hasPath(graph, src, dest, visited));
         // printAllPath(graph, src, dest, visited,""+src);
+
+        //Multisolver Begin
         pq=new PriorityQueue<Pair>(kForkLargest);
-        multisolver(graph, src, dest, visited, "" + src, 0);
-        System.out.println("SmallestPath " + pathWithSmallestWeight + "@" + smallestWeight);
-        System.out.println("LargestPath " + pathWithLargesttWeight + "@" + largestWeight);
-        System.out.println("K Largest " + pq.peek().psf+"@"+pq.peek().wsf);
+        multisolver(graph, src, dest, visited, "" + src, 0,kForkLargest,criteria);
+        System.out.println("Smallest Path = " + pathWithSmallestWeight + "@" + smallestWeight);
+        System.out.println("Largest Path = " + pathWithLargesttWeight + "@" + largestWeight);
+        System.out.println("Just Larger Path than " +criteria+" = " + pathJustLargest + "@" + weightJustLargest);
+        System.out.println("Just Smaller Path than " +criteria+" = " + pathJustSmallest + "@" + weightJustSmallest);
+        System.out.println(kForkLargest+"th largest path = " + pq.peek().psf+"@"+pq.peek().wsf);
     }
 
     private static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] visited) {
@@ -64,7 +86,7 @@ public class Runner {
     }
 
     private static void multisolver(ArrayList<Edge>[] graph, int src, int dest, boolean[] visited, String pathSoFar,
-            int weightSoFar) {
+            int weightSoFar, int kForkLargest, int criteria) {
 
         if (src == dest) {
             if (weightSoFar < smallestWeight) {
@@ -77,6 +99,19 @@ public class Runner {
                 pathWithLargesttWeight = pathSoFar;
 
             }
+            if(weightSoFar > criteria &&weightSoFar <weightJustLargest){
+                weightJustLargest = weightSoFar;
+                pathJustLargest=pathSoFar;
+            }
+
+            if(weightSoFar < criteria &&weightSoFar >weightJustSmallest){
+                weightJustSmallest = weightSoFar;
+                pathJustSmallest=pathSoFar;
+            }
+
+            if(pq.size()<kForkLargest){
+                pq.add(new Pair(weightSoFar, pathSoFar));
+            }else
             if(weightSoFar > pq.peek().wsf)
                 {
                     pq.remove();
@@ -89,7 +124,7 @@ public class Runner {
         for (Edge edge : edges) {
             // Do not visit the neighbours which are visited
             if (visited[edge.nbr] == false)
-                multisolver(graph, edge.nbr, dest, visited, pathSoFar + edge.nbr, weightSoFar + edge.wt);
+                multisolver(graph, edge.nbr, dest, visited, pathSoFar + edge.nbr, weightSoFar + edge.wt,kForkLargest,criteria);
 
         }
 
